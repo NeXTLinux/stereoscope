dc7f9f175394295e4b4c8e2c5e990060ec97a0ae
w3security <99838653+cyberoslab@users.noreply.github.com>
2023-05-21 16:35:58 -0700

    up6

---
+++ test/integration/mime_type_detection_test.go
@@ -0,0 +1,37 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/scylladb/go-set/strset"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/nextlinux/stereoscope"
+	"github.com/nextlinux/stereoscope/pkg/imagetest"
+)
+
+func TestContentMIMETypeDetection(t *testing.T) {
+	request := imagetest.PrepareFixtureImage(t, "docker-archive", "image-simple")
+
+	img, err := stereoscope.GetImage(context.TODO(), request)
+
+	assert.NoError(t, err)
+	t.Cleanup(stereoscope.Cleanup)
+
+	pathsByMIMEType := map[string]*strset.Set{
+		"text/plain": strset.New("/somefile-1.txt", "/somefile-2.txt", "/really", "/really/nested", "/really/nested/file-3.txt"),
+	}
+
+	for mimeType, paths := range pathsByMIMEType {
+		refs, err := img.SquashedSearchContext.SearchByMIMEType(mimeType)
+		assert.NoError(t, err)
+		assert.NotZero(t, len(refs), "found no refs for type=%q", mimeType)
+		for _, ref := range refs {
+			if !paths.Has(string(ref.RealPath)) {
+				t.Errorf("unable to find %q", ref.RealPath)
+			}
+		}
+	}
+
+}
